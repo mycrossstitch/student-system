f95a0e2f7af69037d15a02c2d080d3af884b9f1f
Olga <mycrossstitch@yandex.ru>
2025-10-25 14:47:50 +0300

    Реализована структура проекта и БД

---
+++ src/main/java/ru/system/student/database/Database.java
@@ -0,0 +1,20 @@
+package ru.system.student.database;
+
+import ru.system.student.model.Faculty;
+import ru.system.student.model.Passport;
+import ru.system.student.model.Student;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database {
+
+    public final static  Map<Passport, Student> STUDENTS = new HashMap<>();
+
+    private final static List<Faculty> FACULTIES =
+            List.of(new Faculty("Математический", "+79991111111", "math@ya.ru"),
+                    new Faculty("Физический", "+79992222222", "phis@ya.ru"),
+                    new Faculty("Экономический", "+79993333333", "econom@ya.ru")
+    );
+}
---
+++ src/main/java/ru/system/student/model/Faculty.java
@@ -0,0 +1,19 @@
+package ru.system.student.model;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Faculty {
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String eMail;
+
+}
